7cd2dd4e8159188870536a07459b0a0c3d793de9
Hardita <78241411+Dita-R@users.noreply.github.com>
2026-04-07 12:39:53 -0700

    Teammates main endpoint file

---
+++ backend/api/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from routes import ticker, industry, time, profitability, risk, analysis, etl
+
+app = FastAPI()
+
+app.include_router(ticker.router)
+app.include_router(industry.router)
+app.include_router(time.router)
+app.include_router(profitability.router)
+app.include_router(risk.router)
+app.include_router(analysis.router)
+app.include_router(etl.router)
